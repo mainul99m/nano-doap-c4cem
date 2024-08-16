ec9e071f8137dd8e7426f70e2378b7a9a1d51b5c
Kazi Mainul Islam <mainul99m@gmail.com>
2024-08-16 16:13:19 +0300

    Add splash screen

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:nano_doap_c4cem/controllers/splash_screen_controller.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -9,15 +10,33 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-            'NANO-DOAP C4CEM Splash Screen',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.redAccent
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/common/logo_t.png', fit: BoxFit.contain, height: 150, width: 150),
+                Image.asset('assets/common/logo_nano_doap_white.jpg', fit: BoxFit.contain, height: 100, width: 270),
+              ],
             ),
-        ),
+          ),
+          Positioned(
+            bottom: 8,
+            left: 0,
+            right: 0,
+            child: Text(
+              'version 2.0',
+              style: GoogleFonts.fredoka(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.grey
+              ),
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
       ),
     );
   }
