097a7359c9e08734b932e36243dcb11da197ceaa
Kazi Mainul Islam <mainul99m@gmail.com>
2024-08-31 04:58:08 +0200

    Add image saving functionality

--- lib/services/image_service.dart
+++ lib/services/image_service.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 
 class ImageService {
   static Future<String?> getImagePathFromCamera() async {
@@ -19,5 +22,25 @@ class ImageService {
 
   static Future<bool> deleteImage(String path) async {
     return true;
+  }
+
+  static Future<String> saveImage(File imageFile) async {
+    final appDirectory = await getApplicationDocumentsDirectory();
+    final filePath = appDirectory.path + '/${DateTime.now().millisecondsSinceEpoch}.jpg';
+    await imageFile.copy(filePath);
+    return filePath;
+  }
+
+  Future<void> deleteImageFile(String filePath) async {
+    try {
+      final file = File(filePath);
+      if (await file.exists()) {
+        await file.delete();
+      } else {
+        print('Image file not found: $filePath');
+      }
+    } catch (error) {
+      print('Error deleting image file: $error');
+    }
   }
 }
