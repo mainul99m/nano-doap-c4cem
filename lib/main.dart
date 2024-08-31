efb57aea7a2ea2f604914e10282a3e6227082f1b
Kazi Mainul Islam <mainul99m@gmail.com>
2024-08-31 05:10:26 +0200

    Add hive Boxs

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:nano_doap_c4cem/utils/languages.dart';
+import 'package:nano_doap_c4cem/utils/shared_prefs_constants.dart';
 import 'package:nano_doap_c4cem/views/splash_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -13,6 +15,14 @@ void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   sharedPrefs = await SharedPreferences.getInstance();
   selectedLanguageCode = sharedPrefs.getString('LANGUAGE_CODE') ?? 'en_US';
+
+  await Hive.initFlutter();
+  await Hive.openBox(SharedPrefsConstants.FISH_BOX);
+  await Hive.openBox(SharedPrefsConstants.ENVIRONMENT_BOX);
+  await Hive.openBox(SharedPrefsConstants.PLANKTON_BOX);
+  await Hive.openBox(SharedPrefsConstants.PLASTIC_BOX);
+  await Hive.openBox(SharedPrefsConstants.RESOURCES_BOX);
+
   runApp(const MyApp());
 }
 
--- lib/utils/shared_prefs_constants.dart
+++ lib/utils/shared_prefs_constants.dart
@@ -7,4 +7,11 @@ class SharedPrefsConstants {
   static const String USER_NAME = 'USER_NAME';
   static const String USER_EMAIL = 'USER_EMAIL';
   static const String USER_PHONE = 'USER_PHONE';
+
+  // Hive
+  static const String FISH_BOX = 'FISH_BOX';
+  static const String ENVIRONMENT_BOX = 'ENVIRONMENT_BOX';
+  static const String PLANKTON_BOX = 'PLANKTON_BOX';
+  static const String PLASTIC_BOX = 'PLASTIC_BOX';
+  static const String RESOURCES_BOX = 'RESOURCES_BOX';
 }
